1fa97acff173fbb9e7f4e627354beae651fb08fd
joycebyukusenge1 <joycebyukusenge1@gmail.com>
2024-11-28 19:36:27 +0200

    Add files via upload

---
+++ introduction.jsx
@@ -0,0 +1,24 @@
+  function Intro(){
+
+    const name = "BYUKUSENGE Joyce";
+    const level = "L5sod"
+
+    return(
+        <>
+        
+<div>
+    <h1>name : {name}</h1>
+    <h2>class : {level}</h2>
+
+    
+
+</div>
+
+
+
+        </>
+    )
+
+  }
+
+  export default Intro
